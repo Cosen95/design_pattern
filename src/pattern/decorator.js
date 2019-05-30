33a503c129bf64b3616af2b696a8ea643c3595ca
fengshuan <1263215592@qq.com>
2019-05-30 17:38:06 +0800

    feat:装饰器方法

--- src/pattern/decorator.js
+++ src/pattern/decorator.js
@@ -25,3 +25,32 @@ circle.draw()
 let decorator = new Decorator(circle)
 decorator.draw()
 
+
+// function testDemo(isDecorator) {
+//     return function(target) {
+//         target.isDecorator = isDecorator
+//     }
+// }
+
+// @testDemo(false)
+// class Decorator {
+
+// }
+
+function mixins(...list) {
+    return function(target) {
+        Object.assign(target.prototype, ...list)
+    }
+}
+
+const Foo = {
+    foo() {
+        console.log('foo')
+    }
+}
+@mixins(Foo)
+class Boo {
+
+}
+let bug = new Boo()
+bug.foo()   // foo
